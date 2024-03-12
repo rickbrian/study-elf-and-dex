ada6c60867c74f15623671d74bed66a8b3d19e1e
logan <therick.brian@gmail.com>
2024-03-12 23:37:07 +0800

    push

---
+++ load_elf/jni/main.c
@@ -0,0 +1,21 @@
+#include "load_elf.h"
+
+int main() {
+    // void* handle = load_elf("/data/local/tmp/libfoo1111111111222.so");
+
+    // typedef int*(*PFN_ADD)(int,int);
+
+    // PFN_ADD pAdd = (PFN_ADD)myDlsym(handle,"Add");
+    // if (pAdd)
+    // {
+    //     printf("1 + 3 = %d ",pAdd(1,3));
+    // }
+    
+    // PFN_ADD pSub=  (PFN_ADD)myDlsym(handle,"Sub");
+    // if (pSub)
+    // {
+    //     printf("1 - 3 = %d ",pSub(1,3));
+    // }
+
+    return 0;
+}
---
+++ shell_elf/jni/load_elf.h
@@ -0,0 +1,17 @@
+#ifndef LOAD_ELF_LOAD_ELF_H
+#define LOAD_ELF_LOAD_ELF_H
+
+#include <stdio.h>
+#include <dlfcn.h>
+#include <stdlib.h>
+#include <linux/elf.h>
+#include <elf.h>
+#include <memory.h>
+#include <sys/mman.h>
+
+
+
+void* load_elf(const char* sz);
+
+
+#endif //LOAD_ELF_LOAD_ELF_H
